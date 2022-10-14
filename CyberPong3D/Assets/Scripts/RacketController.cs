419792e2326aa5f5c01fa54b7d817be6cd4fa88d
I. Bugra Aslan <46376529+plaistocene@users.noreply.github.com>
2022-10-14 03:09:40 +0300

    Racket Controller implement

---
+++ CyberPong3D/Assets/Scripts/RacketController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class RacketController : MonoBehaviour
+{
+
+    #region Variables
+
+    [SerializeField] private float _speed;
+
+    [SerializeField] private KeyCode _up;
+    [SerializeField] private KeyCode _down;
+    private bool _moveUp;
+    private bool _moveDown;
+
+    [SerializeField] private Rigidbody _rigidbody;
+
+    #endregion
+
+
+    #region Unity Functions
+
+    private void Update()
+    {
+        _moveUp = Input.GetKey(this._up);
+        _moveDown = Input.GetKey(this._down);
+    }
+
+    private void FixedUpdate()
+    {
+        if (_moveUp)
+        {
+            _rigidbody.velocity = _speed * Time.fixedDeltaTime * Vector3.forward;
+        }
+        else if (_moveDown)
+        {
+            _rigidbody.velocity = _speed * Time.fixedDeltaTime * Vector3.back;
+        }
+        else
+        {
+            _rigidbody.velocity = Vector3.zero;
+        }
+    }
+
+    #endregion
+
+    #region Functions
+    #endregion
+}
