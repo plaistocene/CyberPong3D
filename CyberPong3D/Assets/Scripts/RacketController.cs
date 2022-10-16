92bbbb25103e49126c7efae9e395a449364a0aaa
I. Bugra Aslan <46376529+plaistocene@users.noreply.github.com>
2022-10-16 14:33:48 +0300

    Added an AI controller to RacketController

--- CyberPong3D/Assets/Scripts/RacketController.cs
+++ CyberPong3D/Assets/Scripts/RacketController.cs
@@ -5,14 +5,24 @@ public class RacketController : MonoBehaviour
 
     #region Variables
 
-    [SerializeField] private float _speed;
+    // References
+    [SerializeField] private Rigidbody _racketRigidbody;
+    [SerializeField] private Transform _ballTransform;
 
+    // Gameplay Variables
+    [SerializeField] private float _playerSpeed;
+
+    [SerializeField] private float _aiSpeed = 300f;
+    [SerializeField] private float _offset = 0.2f;
+
+    // Gameplay Control Variables
     [SerializeField] private KeyCode _up;
     [SerializeField] private KeyCode _down;
     private bool _moveUp;
     private bool _moveDown;
 
-    [SerializeField] private Rigidbody _rigidbody;
+    // vs. ai or vs. player controller
+    [SerializeField] private bool _isPlayer = true;
 
     #endregion
 
@@ -21,28 +31,60 @@ public class RacketController : MonoBehaviour
 
     private void Update()
     {
-        _moveUp = Input.GetKey(this._up);
-        _moveDown = Input.GetKey(this._down);
+        if (_isPlayer)
+        {
+            _moveUp = Input.GetKey(this._up);
+            _moveDown = Input.GetKey(this._down);
+        }
     }
 
     private void FixedUpdate()
     {
-        if (_moveUp)
+        if (_isPlayer)
         {
-            _rigidbody.velocity = _speed * Time.fixedDeltaTime * Vector3.forward;
-        }
-        else if (_moveDown)
-        {
-            _rigidbody.velocity = _speed * Time.fixedDeltaTime * Vector3.back;
+            MoveByPlayer();
         }
         else
         {
-            _rigidbody.velocity = Vector3.zero;
+            MoveByComputer();
         }
     }
 
     #endregion
 
     #region Functions
+
+    private void MoveByPlayer()
+    {
+        if (_moveUp)
+        {
+            _racketRigidbody.velocity = _playerSpeed * Time.fixedDeltaTime * Vector3.forward;
+        }
+        else if (_moveDown)
+        {
+            _racketRigidbody.velocity = _playerSpeed * Time.fixedDeltaTime * Vector3.back;
+        }
+        else
+        {
+            _racketRigidbody.velocity = Vector3.zero;
+        }
+    }
+
+    private void MoveByComputer()
+    {
+        if (_ballTransform.position.z > transform.position.z + _offset)
+        {
+            _racketRigidbody.velocity = _aiSpeed * Time.fixedDeltaTime * Vector3.forward;
+        }
+        else if (_ballTransform.position.z < transform.position.z - _offset)
+        {
+            _racketRigidbody.velocity = _playerSpeed * Time.fixedDeltaTime * Vector3.back;
+        }
+        else
+        {
+            _racketRigidbody.velocity = Vector3.zero;
+        }
+    }
+
     #endregion
 }
