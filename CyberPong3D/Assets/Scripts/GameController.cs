e8722be8ada2359e541bfdc6b2af27c20df3aa51
I. Bugra Aslan <46376529+plaistocene@users.noreply.github.com>
2022-10-16 15:42:56 +0300

    Score system implemented

---
+++ CyberPong3D/Assets/Scripts/GameController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private int _scoreLeft = 0;
+    [SerializeField] private int _scoreRight = 0;
+    
+    #endregion
+
+    #region Unity Functions
+    
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+                
+    }
+    
+    #endregion
+
+    #region Custom Functions
+    
+    public void RightScored()
+    {
+        Debug.Log("Right Scored!!!1");
+        _scoreRight++;
+    }
+
+    public void LeftScored()
+    {
+        Debug.Log("Left Scored!!!");
+        _scoreLeft++;
+    }
+    
+    #endregion
+}
---
+++ CyberPong3D/Assets/Scripts/GoalController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GoalController : MonoBehaviour
+{
+    #region Variables
+
+    public UnityEvent onTriggerEnter;
+
+    #endregion
+
+    #region Unity Functions
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            onTriggerEnter.Invoke();
+        }
+    }
+
+    #endregion
+
+    #region Custom Functions
+
+
+
+    #endregion
+}
